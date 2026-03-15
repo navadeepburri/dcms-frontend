232ddda1675d9a970a5250254bced2426049fd26
NavadeepBurri <navadeepburri2005@gmail.com>
2026-03-15 22:35:45 +0530

    Use environment variable for API URL

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8081/api";
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8081/api";
 
 function getToken() {
   const user = localStorage.getItem("dcms_user");
